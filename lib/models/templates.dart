1e2f2839f46ea9a0334e5493b72fbc37fa05f01e
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-02 16:00:14 +0700

    Models

--- lib/models/templates.dart
+++ lib/models/templates.dart
@@ -1,20 +1,20 @@
 part of 'models.dart';
 
 class Templates {
+  final String photo;
   final String name;
   final String desc;
   final String price;
-  final String photo;
   const Templates(
+    this.photo,
     this.name,
     this.desc,
-    this.price,
-    this.photo
+    this.price
   );
   List<Object> get props => [
+    photo,
     name,
     desc,
-    price,
-    photo
+    price
   ];
 }
