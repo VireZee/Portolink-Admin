5ddf5069710e7a3d2ec8a36417dad4c268d5e4c8
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-03 13:13:53 +0700

    Services.dart

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -3,4 +3,5 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:portolink_admin/models/models.dart';
 
+part 'activity.dart';
 part 'auth.dart';
