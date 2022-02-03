f8e25074b89b3dedffaa4d06d85a6094e8530e6a
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-04 01:01:40 +0700

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,35 @@
-import 'package:animated_theme_switcher/animated_theme_switcher.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-}
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    final dark = WidgetsBinding.instance!.window.platformBrightness == Brightness.dark;
-    final initTheme = dark ? BlackWhite.darkTheme() : BlackWhite.lightTheme();
-    return ThemeProvider(
-      initTheme: initTheme,
-      builder: (context, theme) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: theme,
-          initialRoute: '/',
-          routes: {
-            // '/': (context) => const Splash(),
-            // '/in' : (context) => const SignIn(),
-            // '/up': (context) => const SignUp(),
-            // '/main': (context) => const MainMenu()
-          }
-        );
-      }
-    );
-  }
-}
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+//   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+// }
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     final dark = WidgetsBinding.instance!.window.platformBrightness == Brightness.dark;
+//     final initTheme = dark ? BlackWhite.darkTheme() : BlackWhite.lightTheme();
+//     return ThemeProvider(
+//       initTheme: initTheme,
+//       builder: (context, theme) {
+//         return MaterialApp(
+//           debugShowCheckedModeBanner: false,
+//           theme: theme,
+//           initialRoute: '/',
+//           routes: {
+//             // '/': (context) => const Splash(),
+//             // '/in' : (context) => const SignIn(),
+//             // '/up': (context) => const SignUp(),
+//             // '/main': (context) => const MainMenu()
+//           }
+//         );
+//       }
+//     );
+//   }
+// }
