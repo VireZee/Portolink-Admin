5b7718edb8ccdb0634c93247c23af8c48a47d549
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-04 00:53:02 +0700

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
