1e30a5690aa1243d06bb7f0a686c272670066b9c
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-03 13:24:11 +0700

    Services

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -19,7 +19,24 @@ class Auth {
     final String token;
     final String aid;
     try {
-
+      UserCredential aCredential = await auth.createUserWithEmailAndPassword(email: admins.email, password: admins.password);
+      aid = aCredential.user!.uid;
+      token = (await FirebaseMessaging.instance.getToken())!;
+      await aCollection.doc(aid).set({
+        'UID': aid,
+        'Photo': '-',
+        'Name': convertToTitleCase(admins.name),
+        'Email': admins.email.replaceAll(' ', '').toLowerCase(),
+        'Password': sha512.convert(utf8.encode(sha512.convert(utf8.encode(admins.password)).toString())).toString(),
+        'Token': token,
+        'Created': dateNow,
+        'Updated': '-',
+        'Entered': '-',
+        'Left': '-'
+      }).then((value) => msg = 'Signed');
+      auth.currentUser!.updatePhotoURL(admins.photo);
+      auth.currentUser!.updateDisplayName(convertToTitleCase(admins.name));
+      return msg;
     } on FirebaseAuthException catch (e) {
       
     }
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:lottie/lottie.dart';
