d6ebe571cecb326c9fa969a9334c04f82ebfcce4
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-05 18:10:46 +0700

    In

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -1,1 +1,15 @@
 part of 'views.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
