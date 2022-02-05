476cf4c893eb905efc39a30e0d56cb7905e2ca95
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-05 18:21:06 +0700

    Views

--- lib/views/views.dart
+++ lib/views/views.dart
@@ -1,9 +1,8 @@
-part 'signin.dart';
-part 'signup.dart';
 import 'dart:async';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+part 'signin.dart';
+part 'signup.dart';
 part 'splash.dart';
