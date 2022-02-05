a1613f2d3b189aa0160e8d0caf10b23dbca0e779
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-05 18:32:50 +0700

    Menu

--- lib/views/mainmenu.dart
+++ lib/views/mainmenu.dart
@@ -92,3 +92,32 @@ class _MainMenuState extends State<MainMenu> {
     );
   }
 }
+class Custom extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()..shader = const SweepGradient(
+      colors: [
+        Color(0xFF4ADEDE),
+        Color(0xFF1CA7EC)
+      ]
+    ).createShader(Rect.largest)..style = PaintingStyle.fill;
+    final Path path = Path();
+    path.moveTo(0, 20);
+    path.quadraticBezierTo(size.width * 0.20, 0, size.width * 0.35, 0);
+    path.quadraticBezierTo(size.width * 0.40, 0, size.width * 0.40, 20);
+    path.arcToPoint(
+      Offset(size.width * 0.60, 20),
+      radius: const Radius.circular(20.0),
+      clockwise: false
+    );
+    path.quadraticBezierTo(size.width * 0.60, 0, size.width * 0.65, 0);
+    path.quadraticBezierTo(size.width * 0.80, 0, size.width, 20);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 20);
+    canvas.drawShadow(path, Colors.black, 5, true);
+    canvas.drawPath(path, paint);
+  }
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
