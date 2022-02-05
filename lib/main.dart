15ea2eef21360fe875abd24ba233b6efc4915639
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-05 18:22:56 +0700

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:portolink_admin/shared/shared.dart';
+import 'package:portolink_admin/views/views.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
           theme: theme,
           initialRoute: '/',
           routes: {
-            // '/': (context) => const Splash(),
+            '/': (context) => const Splash(),
             // '/in' : (context) => const SignIn(),
             // '/up': (context) => const SignUp(),
             // '/main': (context) => const MainMenu()
