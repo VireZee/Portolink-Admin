8a43e6405642bb7450a6645d2ee8e8c31a3443c6
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-05 18:20:36 +0700

    Splash

--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -7,6 +7,34 @@ class Splash extends StatefulWidget {
   _SplashState createState() => _SplashState();
 }
 class _SplashState extends State<Splash> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final ft = FToast();
+  @override
+  void initState() {
+    super.initState();
+    _loadSplash();
+    ft.init(context);
+  }
+  _loadSplash() async {
+    var _duration = const Duration(seconds: 5);
+    return Timer(_duration, checkAuth);
+  }
+  void checkAuth() async {
+    if (auth.currentUser != null) {
+      final ctrlName = auth.currentUser!.displayName;
+      Navigator.pushReplacementNamed(context, '/main');
+      ft.showToast(
+        child: Activity.showToast(
+          'Welcome Back, ' + ctrlName!.split(' ').first,
+          Colors.blue
+        ),
+        toastDuration: const Duration(seconds: 1),
+        fadeDuration: 200
+      );
+    } else {
+      Navigator.pushReplacementNamed(context, '/in');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
