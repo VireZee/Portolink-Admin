a71450a5a71a2f5972608946a740c0378a13c9e5
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-05 18:02:34 +0700

    Up

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,1 +1,15 @@
 part of 'views.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
