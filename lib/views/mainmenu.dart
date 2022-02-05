e89d390e927fa8104c65664e6c698d5d43bfa59b
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-05 18:30:25 +0700

    Menu

--- lib/views/mainmenu.dart
+++ lib/views/mainmenu.dart
@@ -1,1 +1,24 @@
 part of 'views.dart';
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({Key? key}) : super(key: key);
+  static const String routeName = '/main';
+  @override
+  _MainMenuState createState() => _MainMenuState();
+}
+class _MainMenuState extends State<MainMenu> {
+  static int _selectedIntex = 3;
+  final List<Widget> _widgetOptions = <Widget>[
+    const Home(),
+    const Chat(),
+    const History(),
+    const Profile()
+  ];
+  void _onItemTapped(int index) {
+    setState(() => _selectedIntex = index);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
