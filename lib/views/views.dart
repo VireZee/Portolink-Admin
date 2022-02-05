300a5dc918d392e0c2197001e06173c1ed38a488
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-05 18:08:06 +0700

    Up

--- lib/views/views.dart
+++ lib/views/views.dart
@@ -1,6 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:portolink_admin/models/models.dart';
 import 'package:portolink_admin/services/services.dart';
