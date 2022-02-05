a05094eb8eb05564490291d3de5ca191e765c01a
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-05 18:05:01 +0700

    Up

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -2,12 +2,20 @@ part of 'views.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
-
+  static const String routeName = '/up';
   @override
   _SignUpState createState() => _SignUpState();
 }
-
 class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
+  final ctrlName =  TextEditingController();
+  final ctrlEmail = TextEditingController();
+  final ctrlPass = TextEditingController();
+  final ctrlCPass = TextEditingController();
+  final ft = FToast();
+  static bool vis = true;
+  static bool load = false;
+  static bool btn = true;
   @override
   Widget build(BuildContext context) {
     return Container();
