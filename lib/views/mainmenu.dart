501db1e3fc5b85a670d77927184b36bf32884b0e
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-05 18:32:29 +0700

    Menu

--- lib/views/mainmenu.dart
+++ lib/views/mainmenu.dart
@@ -19,5 +19,76 @@ class _MainMenuState extends State<MainMenu> {
   } 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(child: _widgetOptions.elementAt(_selectedIntex)),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: SizedBox(
+              width: size.width,
+              height: 80,
+              child: Stack(
+                children: [
+                  CustomPaint(  
+                    size: Size(size.width, 80),
+                    painter: Custom()
+                  ),
+                  Center(
+                    heightFactor: 0.6,
+                    child: FloatingActionButton(
+                      backgroundColor: Colors.blue,
+                      child: const Icon(Icons.shopping_bag),
+                      elevation: 0.1,
+                      onPressed: () {}
+                    )
+                  ),
+                  SizedBox(
+                    width: size.width,
+                    height: 80,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.home,
+                            color: _selectedIntex == 0 ? Colors.blue : null
+                          ),
+                          onPressed: () =>_onItemTapped(0)
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.chat,
+                            color: _selectedIntex == 1 ? Colors.blue : null
+                          ),
+                          onPressed: () => _onItemTapped(1)
+                        ),
+                        Container(width: size.width * 0.20),
+                        IconButton(
+                          icon: Icon(
+                            Icons.history,
+                            color: _selectedIntex == 2 ? Colors.blue : null
+                          ),
+                          onPressed: () => _onItemTapped(2)
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.person,
+                            color : _selectedIntex == 3 ? Colors.blue : null
+                          ),
+                          onPressed: () => _onItemTapped(3)
+                        )
+                      ]
+                    )
+                  )
+                ]
+              )
+            )
+          )
+        ]
+      )
+    );
   }
 }
