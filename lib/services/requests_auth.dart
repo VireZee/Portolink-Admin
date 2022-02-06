fcbc087abf5abfc2332cdb093d4010847b004ba3
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-06 13:47:16 +0700

    ReqAuth

--- lib/services/requests_auth.dart
+++ lib/services/requests_auth.dart
@@ -1,5 +1,37 @@
 part of 'services.dart';
 
 class RequestAuth {
-
+  static FirebaseAuth auth = FirebaseAuth.instance;
+  static CollectionReference rCollection = FirebaseFirestore.instance.collection('Orders');
+  static DocumentReference? rDocument;
+  static Reference? ref;
+  static UploadTask? uploadTask;
+  static String? imgUrl;
+  static Future<bool> approved() async {
+    await Firebase.initializeApp();
+    await rCollection.doc(rDocument!.id).update({
+      'Pending Status': {
+        'Status': 'Approved, Waiting for payment'
+      }
+    });
+    return true;
+  }
+  static Future<bool> rejected() async {
+    await Firebase.initializeApp();
+    await rCollection.doc(rDocument!.id).update({
+      'Pending Status': {
+        'Status': 'Rejected'
+      }
+    });
+    return true;
+  }
+  static Future<bool> finished() async {
+    await Firebase.initializeApp();
+    await rCollection.doc(rDocument!.id).update({
+      'Pending Status': {
+        'Status': 'Finished'
+      }
+    });
+    return true;
+  }
 }
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -4,6 +4,7 @@ import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:lottie/lottie.dart';
