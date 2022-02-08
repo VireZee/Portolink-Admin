61da4117ce4ac48b679c65a85e2e3f50f3e0f2c1
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-08 20:33:29 +0700

    Profileview

--- lib/widgets/profileview.dart
+++ lib/widgets/profileview.dart
@@ -18,6 +18,254 @@ class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
     final Admins admins = widget.admins;
-    return Container();
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/profile_bg.jpg'),
+          fit: BoxFit.fill
+        )
+      ),
+      child: Stack(
+        children: [
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              toolbarHeight: 75,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actions: [
+                const Spacer(flex: 25),
+                ThemeSwitcher(
+                  builder: (context) => IconButton(
+                    onPressed: () {
+                      setState(() => c = !c);
+                      ThemeSwitcher.of(context).changeTheme(
+                        theme: ThemeModelInheritedNotifier.of(context).theme.brightness == Brightness.dark
+                        ? BlackWhite.lightTheme()
+                        : BlackWhite.darkTheme()
+                      );
+                    },
+                    icon: c ? const Icon(CupertinoIcons.sun_max_fill) : const Icon(CupertinoIcons.moon_stars_fill),
+                    color: c ? Colors.white : Colors.black
+                  )
+                ),
+                const Spacer()
+              ]
+            ),
+            body: ListView(
+              physics: const BouncingScrollPhysics(),
+              children: [
+                Column(
+                  children: [
+                    ProfilePicture(
+                      name: admins.name,
+                      radius: 75,
+                      fontsize: 50,
+                      random: true,
+                      tooltip: true,
+                      role: ''
+                    ),
+                    Text(
+                      admins.name,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontFamily: 'Oswald', fontSize: 30)
+                    ),
+                    Text(
+                      admins.email,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: Colors.blue, decoration: TextDecoration.underline, decorationThickness: 2)
+                    ),
+                    const SizedBox(height: 24),
+                    ElevatedButton(
+                      onPressed: () async {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Update(
+                              name: admins.name,
+                              email: admins.email
+                            )
+                          )
+                        );
+                      },
+                      child: const Text('Edit Profile'),
+                      style: ButtonStyle(
+                        overlayColor: MaterialStateProperty.resolveWith((states) {
+                          return states.contains(MaterialState.pressed)
+                          ? Colors.blue
+                          : null;
+                        }),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                        ),
+                        padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 12, horizontal: 32))
+                      )
+                    ),
+                    const SizedBox(height: 70),
+                    Material(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Help(),
+                            )
+                          );
+                        },
+                        child: SizedBox(
+                          width: 350,
+                          height: 60,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Spacer(),
+                              Icon(Icons.help),
+                              Spacer(flex: 6),
+                              Text('Help'),
+                              Spacer(flex: 6),
+                              Icon(Icons.keyboard_arrow_right_sharp),
+                              Spacer()
+                            ]
+                          )
+                        ),
+                        highlightColor: Colors.blue,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      color: Colors.grey.withOpacity(0.6),
+                      shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(50))
+                    ),
+                    const SizedBox(height: 12),
+                    Material(
+                      child: InkWell(
+                        onTap: () async {
+                          setState(() => load = true);
+                          final net = await (Connectivity().checkConnectivity());
+                          final sub = await InternetConnectionChecker().hasConnection;
+                          if (net == ConnectivityResult.none) {
+                            setState(() => load = false);
+                            ft.showToast(
+                              child: Activity.showToast(
+                                'No internet connection',
+                                const Color(0xFFFF0000)
+                              ),
+                              toastDuration: const Duration(seconds: 1),
+                              fadeDuration: 200
+                            );
+                          }
+                          else if (sub) {
+                            await Auth.signOut().then((value) {
+                              setState(() => load = false);
+                              Navigator.pushReplacementNamed(context, '/in');
+                            });
+                          } else {
+                            setState(() => load = false);
+                            ft.showToast(
+                              child: Activity.showToast(
+                                'No internet connection',
+                                const Color(0xFFFF0000)
+                              ),
+                              toastDuration: const Duration(seconds: 1),
+                              fadeDuration: 200
+                            );
+                          }
+                        },
+                        child: SizedBox(
+                          width: 350,
+                          height: 60,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Spacer(),
+                              Icon(Icons.logout),
+                              Spacer(flex: 5),
+                              Text('Sign Out'),
+                              Spacer(flex: 5),
+                              Icon(Icons.keyboard_arrow_right_sharp),
+                              Spacer()
+                            ]
+                          )
+                        ),
+                        highlightColor: Colors.blue,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      color: Colors.grey.withOpacity(0.6),
+                      shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(50))
+                    ),
+                    const SizedBox(height: 12),
+                    Material(
+                      child: InkWell(
+                        onTap: () async {
+                          setState(() => load = true);
+                          final net = await (Connectivity().checkConnectivity());
+                          final sub = await InternetConnectionChecker().hasConnection;
+                          if (net == ConnectivityResult.none) {
+                            setState(() => load = false);
+                            ft.showToast(
+                              child: Activity.showToast(
+                                'No internet connection',
+                                const Color(0xFFFF0000)
+                              ),
+                              toastDuration: const Duration(seconds: 1),
+                              fadeDuration: 200
+                            );
+                          }
+                          else if (sub) {
+                            await Auth.deleteAccount().then((value) {
+                              setState(() => load = false);
+                              ft.showToast(
+                                child: Activity.showToast(
+                                  'Goodbye',
+                                  Colors.blue
+                                ),
+                                toastDuration: const Duration(seconds: 1),
+                                fadeDuration: 200
+                              );
+                              Navigator.pushReplacementNamed(context, '/in');
+                            });
+                          } else { 
+                            setState(() => load = false);
+                            ft.showToast(
+                              child: Activity.showToast(
+                                'No internet connection',
+                                const Color(0xFFFF0000)
+                              ),
+                              toastDuration: const Duration(seconds: 1),
+                              fadeDuration: 200
+                            );
+                          }
+                        },
+                        child: SizedBox(
+                          width: 350,
+                          height: 60,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Spacer(),
+                              Icon(Icons.clear),
+                              Spacer(flex: 5),
+                              Text('Delete Account'),
+                              Spacer(flex: 5),
+                              Icon(Icons.keyboard_arrow_right_sharp),
+                              Spacer()
+                            ]
+                          )
+                        ),
+                        highlightColor: const Color(0xFFFF0000),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      color: Colors.grey.withOpacity(0.6),
+                      shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(50))
+                    )
+                  ]
+                )
+              ]
+            )
+          ),
+          load == true
+          ? Activity.loading()
+          : Container()
+        ]
+      )
+    );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,5 +1,12 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_profile_picture/flutter_profile_picture.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:portolink_admin/models/models.dart';
+import 'package:portolink_admin/services/services.dart';
+import 'package:portolink_admin/shared/shared.dart';
 
 part 'profileview.dart';
