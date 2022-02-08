7fa67fd67a8eb901583627361eb55df9cd4eb7af
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-08 20:42:35 +0700

    Update

--- lib/views/update.dart
+++ lib/views/update.dart
@@ -8,7 +8,28 @@ class Update extends StatefulWidget {
   _UpdateState createState() => _UpdateState();
 }
 class _UpdateState extends State<Update> {
+  final _formKey = GlobalKey<FormState>();
+  final ctrlName = TextEditingController();
+  final ctrlEmail = TextEditingController();
+  final ft = FToast();
+  static bool load = false;
+  final CollectionReference uCollection = FirebaseFirestore.instance.collection('Users');
+  @override
+  void initState() {
+    ctrlName.text =  widget.name;
+    ctrlEmail.text = widget.email;
+    super.initState();
+    ft.init(context);
+  }
+  @override
+  void dispose() {
+    ctrlName.dispose();
+    ctrlEmail.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
+    final brightness = ThemeModelInheritedNotifier.of(context).theme.brightness;
+    return ThemeSwitchingArea()
   }
 }
--- lib/views/views.dart
+++ lib/views/views.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
