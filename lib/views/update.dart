197e71eb577c14e995d5e5eeae30a8aadac5d5ad
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-08 20:48:02 +0700

    Update

--- lib/views/update.dart
+++ lib/views/update.dart
@@ -30,6 +30,252 @@ class _UpdateState extends State<Update> {
   @override
   Widget build(BuildContext context) {
     final brightness = ThemeModelInheritedNotifier.of(context).theme.brightness;
-    return ThemeSwitchingArea()
+    return ThemeSwitchingArea(
+      child: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/edit_bg.jpg'),
+                fit: BoxFit.fill
+              )
+            ),
+            child: Scaffold(
+              appBar: AppBar(
+                leading: const BackButton(),
+                backgroundColor: Colors.transparent,
+                toolbarHeight: 75,
+                elevation: 0,
+                actions: [
+                  ThemeSwitcher(
+                    builder: (context) => IconButton(
+                      onPressed: () {
+                        ThemeSwitcher.of(context).changeTheme(
+                          theme: ThemeModelInheritedNotifier.of(context).theme.brightness == Brightness.dark
+                          ? BlackWhite.lightTheme()
+                          : BlackWhite.darkTheme()
+                        );
+                      },
+                      icon: brightness == Brightness.dark ? const Icon(CupertinoIcons.sun_max_fill) : const Icon(CupertinoIcons.moon_stars_fill),
+                      color: brightness == Brightness.dark ? Colors.white : Colors.black
+                    )
+                  )
+                ]
+              ),
+              backgroundColor: Colors.transparent,
+              body: Stack(
+                children: [
+                  ListView(
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    physics: const BouncingScrollPhysics(),
+                    children: [
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Name',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 25
+                              )
+                            ),
+                            const SizedBox(height: 8),
+                            TextField(
+                              controller: ctrlName,
+                              keyboardType: TextInputType.name,
+                              style: const TextStyle(
+                                fontFamily: 'Flamenco',
+                                fontSize: 30
+                              ),
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(borderRadius: BorderRadius.circular(12))
+                              ),
+                              maxLines: 1
+                            ),
+                            const SizedBox(height: 16),
+                            const Text(
+                              'Phone',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 25
+                              )
+                            ),
+                            const SizedBox(height: 8),
+                            TextField(
+                              controller: ctrlEmail,
+                              keyboardType: TextInputType.emailAddress,
+                              style: const TextStyle(
+                                fontFamily: 'Flamenco',
+                                fontSize: 30
+                              ),
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(borderRadius: BorderRadius.circular(12))
+                              ),
+                              maxLines: 1
+                            ),
+                            const SizedBox(height: 80),
+                            Center(
+                              child: Material(
+                                child: InkWell(
+                                  onTap: () async {
+                                    setState(() => load = true);
+                                    FocusScope.of(context).requestFocus(FocusNode());
+                                    final net = await (Connectivity().checkConnectivity());
+                                    final sub = await InternetConnectionChecker().hasConnection;
+                                    if (net == ConnectivityResult.none) {
+                                      setState(() => load = false);
+                                      ft.showToast(
+                                        child: Activity.showToast(
+                                          'No internet connection',
+                                          const Color(0xFFFF0000)
+                                        ),
+                                        toastDuration: const Duration(seconds: 1),
+                                        fadeDuration: 200
+                                      );
+                                    }
+                                    else if (ctrlName.text.isEmpty) {
+                                      setState(() => load = false);
+                                      ft.showToast(
+                                        child: Activity.showToast(
+                                          'Name can\'t be empty',
+                                          const Color(0xFFFF0000)
+                                        ),
+                                        toastDuration: const Duration(seconds: 1),
+                                        fadeDuration: 200
+                                      );
+                                    }
+                                    else if (ctrlEmail.text.isEmpty) {
+                                      setState(() => load = false);
+                                      ft.showToast(
+                                        child: Activity.showToast(
+                                          'Email can\'t be empty',
+                                          const Color(0xFFFF0000)
+                                        ),
+                                        toastDuration: const Duration(seconds: 1),
+                                        fadeDuration: 200
+                                      );
+                                    }
+                                    else if (sub) {
+                                      if (_formKey.currentState!.validate()) {
+                                        final Admins admins = Admins(
+                                          '',
+                                          ctrlName.text,
+                                          ctrlEmail.text,
+                                          ''
+                                        );
+                                        final String msg = await Auth.updateAccount(admins);
+                                        if (msg == 'Granted') {
+                                          setState(() => load = false);
+                                          ft.showToast(
+                                            child: Activity.showToast(
+                                              'Updated',
+                                              Colors.blue
+                                            ),
+                                            toastDuration: const Duration(seconds: 1),
+                                            fadeDuration: 200
+                                          );
+                                        }
+                                        else if (msg == 'Existed') {
+                                          setState(() => load = false);
+                                          ft.showToast(
+                                            child: Activity.showToast(
+                                              'Email is linked to another account',
+                                              const Color(0xFFFF0000)
+                                            ),
+                                            toastDuration: const Duration(seconds: 1),
+                                            fadeDuration: 200
+                                          );
+                                        }
+                                        else if (msg == 'Invalid Email') {
+                                          setState(() => load = false);
+                                          ft.showToast(
+                                            child: Activity.showToast(
+                                              'Email is invalid',
+                                              const Color(0xFFFF0000)
+                                            ),
+                                            toastDuration: const Duration(seconds: 1),
+                                            fadeDuration: 200
+                                          );
+                                        }
+                                        else if (msg == 'Relog') {
+                                          setState(() => load = false);
+                                          ft.showToast(
+                                            child: Activity.showToast(
+                                              'You need to relog',
+                                              const Color(0xFFFF0000)
+                                            ),
+                                            toastDuration: const Duration(seconds: 1),
+                                            fadeDuration: 200
+                                          );
+                                        }
+                                      }
+                                    } else {
+                                      setState(() => load = false);
+                                      ft.showToast(
+                                        child: Activity.showToast(
+                                          'No internet connection',
+                                          const Color(0xFFFF0000)
+                                        ),
+                                        toastDuration: const Duration(seconds: 1),
+                                        fadeDuration: 200
+                                      );
+                                    }
+                                  },
+                                  child: Container(
+                                    height: 40,
+                                    width: 200,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      gradient: const SweepGradient(
+                                        colors: [
+                                          Color(0xFF00FF00),
+                                          Colors.cyan,
+                                          Color(0xFF0000FF),
+                                          Colors.purple,
+                                          Color(0xFFFF0000),
+                                          Colors.yellow,
+                                          Colors.yellow,
+                                          Color(0xFFFF0000),
+                                          Colors.purple,
+                                          Color(0xFF0000FF),
+                                          Colors.cyan,
+                                          Color(0xFF00FF00)
+                                        ]
+                                      )
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: const [
+                                        Spacer(),
+                                        Icon(Icons.update),
+                                        Spacer(flex: 4),
+                                        Text('Update Profile'),
+                                        Spacer(flex: 5),
+                                        Spacer()
+                                      ]
+                                    )
+                                  ),
+                                  borderRadius: BorderRadius.circular(50)
+                                ),
+                                shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(50))
+                              )
+                            )
+                          ]
+                        )
+                      )
+                    ]
+                  )
+                ]
+              )
+            )
+          ),
+          load == true
+          ? Activity.loading()
+          : Container()
+        ]
+      )
+    );
   }
 }
--- lib/views/views.dart
+++ lib/views/views.dart
@@ -3,6 +3,7 @@ import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -10,6 +11,7 @@ import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:lottie/lottie.dart';
 import 'package:portolink_admin/models/models.dart';
 import 'package:portolink_admin/services/services.dart';
+import 'package:portolink_admin/shared/shared.dart';
 import 'package:portolink_admin/widgets/widgets.dart';
 
 part 'chat.dart';
