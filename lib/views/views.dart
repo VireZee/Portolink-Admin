b6a23cbe328870739652ec29ed5b1688a378ee67
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-12 19:59:41 +0700

    Views

--- lib/views/views.dart
+++ lib/views/views.dart
@@ -14,6 +14,7 @@ import 'package:portolink_admin/services/services.dart';
 import 'package:portolink_admin/shared/shared.dart';
 import 'package:portolink_admin/widgets/widgets.dart';
 
+part 'catalog.dart';
 part 'chat.dart';
 part 'home.dart';
 part 'mainmenu.dart';
