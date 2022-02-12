d967774af0be531825a235e294f957eef448b92f
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-12 14:57:00 +0700

    Templates

--- lib/services/templates.dart
+++ lib/services/templates.dart
@@ -3,4 +3,8 @@ part of 'services.dart';
 class Templates {
   static final FirebaseAuth auth = FirebaseAuth.instance;
   static final CollectionReference rCollection = FirebaseFirestore.instance.collection('Templates');
+  static DocumentReference? rDocument;
+  static Reference? ref;
+  static UploadTask? uploadTask;
+  static String? imgUrl;
 }
