2565b667e5e5da1281a23d140aabdad9ab3d0e2d
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-14 02:17:45 +0700

    Formatting Models

--- lib/models/pendings.dart
+++ lib/models/pendings.dart
@@ -2,10 +2,6 @@ part of 'models.dart';
 
 class Pendings {
   final String text;
-  const Pendings(
-    this.text
-  );
-  List<Object> get props => [
-    text
-  ];
+  const Pendings(this.text);
+  List<Object> get props => [text];
 }
--- lib/models/templates.dart
+++ lib/models/templates.dart
@@ -4,14 +4,6 @@ class Templates {
   final String name;
   final String desc;
   final String price;
-  const Templates(
-    this.name,
-    this.desc,
-    this.price
-  );
-  List<Object> get props => [
-    name,
-    desc,
-    price
-  ];
+  const Templates(this.name, this.desc, this.price);
+  List<Object> get props => [name, desc, price];
 }
