8cc00c80a14bdf928c3367d1dc323effbb3fd407
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-14 16:45:59 +0700

    Deleted unused codes

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -6,7 +6,6 @@ class Profile extends StatefulWidget {
   _ProfileState createState() => _ProfileState();
 }
 class _ProfileState extends State<Profile> {
-  final FirebaseAuth auth = Auth.auth;
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
