d5fe68f8a7380306b67eb26a64842d235c20706c
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-14 17:25:02 +0700

    Stf

--- lib/widgets/homeview.dart
+++ lib/widgets/homeview.dart
@@ -1,1 +1,13 @@
 part of 'widgets.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
