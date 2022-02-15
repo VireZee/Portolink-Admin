0e15d462d607ce6eab8a7142d8996959bce409e0
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-15 15:09:55 +0700

    Display

--- lib/widgets/homeview.dart
+++ lib/widgets/homeview.dart
@@ -9,6 +9,35 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Templates templates = widget.templates;
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: ListView(
+        children: [
+          const SizedBox(height: 15.0),
+          Container(
+            padding: const EdgeInsets.only(right: 15),
+            width: size.width - 30,
+            height: size.height - 50,
+            child: GridView.count(
+              crossAxisCount: 2,
+              primary: false,
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 15.0,
+              childAspectRatio: 0.8,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: InkWell(
+                    onTap: () {},
+                  )
+                )
+              ]
+            )
+          )
+        ]
+      )
+    );
   }
 }
