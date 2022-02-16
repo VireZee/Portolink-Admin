f3efd3ba88ba5eba126d4055345bf79441702b1b
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-17 01:26:29 +0700

    Stf

---
+++ lib/views/edit.dart
@@ -0,0 +1,13 @@
+part of 'views.dart';
+
+class Edit extends StatefulWidget {
+  const Edit({Key? key}) : super(key: key);
+  @override
+  _EditState createState() => _EditState();
+}
+class _EditState extends State<Edit> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
