0ab7d66d1be83e42a62f11a72c9ada17ffcf3fb8
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-16 18:56:54 +0700

    Display

--- lib/views/details.dart
+++ lib/views/details.dart
@@ -1,13 +1,17 @@
 part of 'views.dart';
 
 class Details extends StatefulWidget {
-  const Details({Key? key}) : super(key: key);
+  const Details({Key? key, required this.photo, required this.name, required this.desc}) : super(key: key);
+  final String photo;
+  final String name;
+  final String desc;
   @override
   _DetailsState createState() => _DetailsState();
 }
 class _DetailsState extends State<Details> {
+  final CollectionReference tCollection = TemplatesAuth.tCollection;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView();
   }
 }
