8970989a48cbd07539d8d8de077c2266ab6184d7
unknown <jemmanuel@student.ciputra.ac.id>
2022-02-17 12:52:40 +0700

    Key variables

--- lib/views/edit.dart
+++ lib/views/edit.dart
@@ -1,11 +1,66 @@
 part of 'views.dart';
 
 class Edit extends StatefulWidget {
-  const Edit({Key? key}) : super(key: key);
+  const Edit({Key? key, required this.photo, required this.name, required this.desc, required this.price}) : super(key: key);
+  final String photo;
+  final String name;
+  final String desc;
+  final String price;
   @override
   _EditState createState() => _EditState();
 }
 class _EditState extends State<Edit> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController ctrlName = TextEditingController();
+  final TextEditingController ctrlDesc = TextEditingController();
+  final TextEditingController ctrlPrice = TextEditingController();
+  final ImagePicker imagePicker = ImagePicker();
+  XFile? imgFile;
+  final FToast ft = FToast();
+  bool load = false;
+  bool btn = true;
+  Future<dynamic> chooseFile() async {
+    const ImageSource imgSrc = ImageSource.gallery;
+    final XFile? selectedImage = await imagePicker.pickImage(
+      source: imgSrc,
+      imageQuality: 100
+    );
+    setState(() => imgFile = selectedImage);
+  }
+  bool isEmpty() {
+    setState(() {
+      if (ctrlName.text != '' && ctrlDesc.text != '' && ctrlPrice.text != '' && imgFile != null) {
+        btn = true;
+      }
+      else {
+        btn = false;
+      }
+    });
+    return btn;
+  }
+  @override
+  void initState() {
+    ctrlName.text = widget.name;
+    ctrlDesc.text = widget.desc;
+    ctrlPrice.text = widget.price;
+    super.initState();
+    ft.init(context);
+  }
+  @override
+  void dispose() {
+    ctrlName.dispose();
+    ctrlDesc.dispose();
+    ctrlPrice.dispose();
+    super.dispose();
+  }
+  void clearForm() {
+    ctrlName.clear();
+    ctrlDesc.clear();
+    ctrlPrice.clear();
+    setState(() {
+      imgFile = null;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
